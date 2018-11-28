a704ee604d340d9da40c252f0a624a8610d0d349
Allan Weber <a.cassianoweber@gmail.com>
2018-11-28 19:57:51 -0200

    Data

---
+++ Section-6-Creating-Production-Grade-Workflow/frontend/build/precache-manifest.4e8c586fc0c65d74a5521d38e3baa1f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "821c228aab60a14267b8",
+    "url": "/static/js/main.821c228a.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "821c228aab60a14267b8",
+    "url": "/static/css/main.f01df6de.chunk.css"
+  },
+  {
+    "revision": "be2ffc1cc0f9c367c0159db34c304434",
+    "url": "/index.html"
+  }
+];
